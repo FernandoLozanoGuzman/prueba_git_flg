52fa5cebd53c05b07410f0a981e016b9b4e0d11f
flozanog <flozanog@emeal.nttdata.com>
2022-03-29 14:40:51 +0200

    cambios en el main

--- src/prueba_git/Main.java
+++ src/prueba_git/Main.java
@@ -3,6 +3,6 @@ package prueba_git;
 public class Main {
 	public Main() {
 		// TODO Auto-generated constructor stub
-		System.out.println("Hola mundo!!");
+		System.out.println("Hola mundo!! - cambio");
 	}
 }
